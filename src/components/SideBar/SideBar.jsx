7f88ece1c0cdb3759ac3f6f8e79ca1f64dd012e3
Nderitu10 <raynderitu10@gmail.com>
2025-01-21 14:21:50 +0300

    Initial commit

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,13 +2,36 @@
  import './SideBar.css'
  import { assets } from "../../assets/assets"
  
- const sideBar = () => {
+ const SideBar = () => {
    return (
-     <div className="sideBar">
+     <div className="sidebar">
         <div className="top">
-            <img src={assets.menu} alt="" />
+            <img className="menu" src={assets.menu} alt="" />
+            <div className="new-chat">
+              <img src={assets.plus} alt="" />
+              <p>New Chat</p>
+            </div>
+            <div className="recent">
+              <p className="recent-title">Recent</p>
+              <div className="recent-entry">
+                <img src={assets.comment} alt="" />
+                <p>What is React?</p>
+              </div>
+            </div>
         </div>
         <div className="bottom">
+          <div className="bottom-item recent-entry">
+            <img src={assets.question} alt="" />
+            <p>Help</p>
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.history} alt="" />
+            <p>Activity</p>
+          </div>
+          <div className="bottom-item recent-entry">
+            <img src={assets.setting} alt="" />
+            <p>Settings</p>
+          </div>
 
         </div>
  
@@ -16,4 +39,4 @@
    )
  }
  
- export default sideBar
+ export default SideBar

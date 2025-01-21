4c3a86826ee1b7b17e4fe0adcff6db0cd6015273
Nderitu10 <raynderitu10@gmail.com>
2025-01-21 15:10:46 +0300

    Initial commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user} alt=""/>
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p><span>Hello & Welcome!</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest a good travel location for a family trip</p>
+            <img src={assets.compass} alt="" />
+          </div>
+          <div className="card">
+            <p>Summarize the theory or relativity</p>
+            <img src={assets.lightbulb} alt="" />
+          </div>
+          <div className="card">
+            <p>Come up with team building activities for our work retreat</p>
+            <img src={assets.comment} alt="" />
+            <div className="card">
+            <p>Debug the following code</p>
+            <img src={assets.code} alt="" />
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,42 +1,44 @@
- import React from "react"
- import './SideBar.css'
- import { assets } from "../../assets/assets"
- 
- const SideBar = () => {
-   return (
-     <div className="sidebar">
-        <div className="top">
-            <img className="menu" src={assets.menu} alt="" />
-            <div className="new-chat">
-              <img src={assets.plus} alt="" />
-              <p>New Chat</p>
-            </div>
-            <div className="recent">
-              <p className="recent-title">Recent</p>
-              <div className="recent-entry">
-                <img src={assets.comment} alt="" />
-                <p>What is React?</p>
-              </div>
-            </div>
-        </div>
-        <div className="bottom">
-          <div className="bottom-item recent-entry">
-            <img src={assets.question} alt="" />
-            <p>Help</p>
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.history} alt="" />
-            <p>Activity</p>
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.setting} alt="" />
-            <p>Settings</p>
-          </div>
+import React, { useState } from "react";
+import "./SideBar.css";
+import { assets } from "../../assets/assets";
 
+const SideBar = () => {
+  const [extended, setExtended] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img onClick={() => setExtended(prev=>!prev)} className="menu" src={assets.menu} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus} alt="" />
+          {extended ? <p>New Chat</p> : null}
         </div>
- 
-     </div>
-   )
- }
- 
- export default SideBar
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.comment} alt="" />
+              <p>What is React?</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question} alt="" />
+          {extended? <p>Help</p>:null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history} alt="" />
+          {extended? <p>Activity</p>:null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting} alt="" />
+          {extended? <p>Settings</p>:null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;

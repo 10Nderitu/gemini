071d0c3d70e06328f08f263b1c3533c35a7ca8d8
Nderitu10 <raynderitu10@gmail.com>
2025-01-22 10:18:53 +0300

    Initial commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 const Main = () => {
+
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -11,41 +23,50 @@ const Main = () => {
       </div>
 
       <div className="main-container">
+
         <div className="greet">
           <p>
             <span>Hello & Welcome!</span>
           </p>
           <p>How can I help you today?</p>
         </div>
+
         <div className="cards">
+          
           <div className="card">
             <p>Suggest a good travel location for a family trip</p>
             <img src={assets.compass} alt="" />
           </div>
+
           <div className="card">
             <p>Summarize the theory or relativity</p>
             <img src={assets.lightbulb} alt="" />
           </div>
+
           <div className="card">
             <p>Come up with team building activities for our work retreat</p>
             <img src={assets.comment} alt="" />
           </div>
+
           <div className="card">
             <p>Debug the following code</p>
             <img src={assets.code} alt="" />
           </div>
+
         </div>
 
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Ask Gemini" />
+            <input onChange = {(e) => setInput(e.target.value)} value = {input} type="text" placeholder="Ask Gemini" />
             <img src={assets.gallery} alt="" />
             <img src={assets.mic} alt="" />
-            <img src={assets.send} alt="" />
+            <img src={assets.send} alt="" />  
           </div>
+
           <p className="bottom-info">
             Gemini may display inaccurate info, so double check its responses.
           </p>
+          
         </div>
       </div>
     </div>

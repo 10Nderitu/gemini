3d9dc666a0d68d43d5c78ef8dbd80583c464794e
Nderitu10 <raynderitu10@gmail.com>
2025-01-23 15:31:52 +0300

    Fixed API response bugs

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,17 +12,35 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
-    const onSent = async (prompt) => {
+    const delayPara = (index,nextWord) => {
 
-        setResultData("")
-        setLoading(true)
-        setShowResult(true)
-        setRecentPrompt(input)
-        const response = await run(input)
-        setResultData(response)
-        setLoading(false)
-        setInput("")
-        
+    }
+
+    const onSent = async (prompt) => {
+      setResultData("");
+      setLoading(true);
+      setShowResult(true);
+      setRecentPrompt(input);
+      const response = await run(input);
+
+      console.log(typeof response);
+
+      let responseArray = response().split("**");
+      let newResponse = ""; 
+
+      for (let i = 0; i < responseArray.length; i++) {
+        // Check if the current word is surrounded by **
+        if (i % 2 === 1) {
+          newResponse += "<b>" + responseArray[i] + "</b>"; // Wrap in <b> tags
+        } else {
+          newResponse += responseArray[i]; // Just add the plain text
+        }
+      }
+                                                               //split divides a string into an array of substrings
+      let newResponse2 = newResponse.split("*").join("</br>") // join takes an array and a separator as arguments and returns a string
+      setResultData(newResponse2);
+      setLoading(false);
+      setInput("");
     }
 
 

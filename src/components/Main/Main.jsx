5ab3c0307bf67029c21172652713c4845f5bc698
Nderitu10 <raynderitu10@gmail.com>
2025-01-28 20:17:43 +0300

    State management

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
   return (
     <div className="main">
       <div className="nav">
-        <p>Gemini</p>
+        <p>Gemini AI</p>
         <img src={assets.user} alt="" />
       </div>
 
